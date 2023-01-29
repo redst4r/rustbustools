845cff9cc5079d5cd82cd44f8a33b39ad3f3110e
redst4r <redst4r@web.de>
2023-01-29 02:43:10 -0800

    added bayesian counting

--- src/count2.rs
+++ src/count2.rs
@@ -1,16 +1,22 @@
 use std::collections::{HashMap, HashSet, BTreeSet};
 use std::fs::File;
 use std::time::Instant;
+use sprs::DenseVector;
 use sprs::io::write_matrix_market;
 
 use crate::consistent_genes::{find_consistent, Ec2GeneMapper};
 use crate::iterators::CbUmiIterator;
-use crate::io::BusFolder;
+use crate::io::{BusFolder, BusRecord};
+use crate::multinomial::multinomial_sample;
 use crate::utils::{get_progressbar, int_to_seq};
 
 use std::io::Write;
 
 pub struct CountMatrix{
+    /*
+    Represents a count matrix of Cells vs Genes
+    Cells are encoded a Strings already!
+    */
     pub matrix: sprs::CsMat<usize>,
     pub cbs: Vec<String>,
     pub genes: Vec<String>,
@@ -46,7 +52,7 @@ impl CountMatrix {
     }
 }
 
-fn countmap_to_matrix(countmap: &HashMap<(u64, String), usize>, gene_vector: Vec<&String>) -> CountMatrix{
+fn countmap_to_matrix(countmap: &HashMap<(u64, u32), usize>, gene_vector: Vec<String>) -> CountMatrix{
 
     // get all CBs, a BTreeSet gives us order for free
     // let cb_set: BTreeSet<u64> = BTreeSet::new();
@@ -57,24 +63,23 @@ fn countmap_to_matrix(countmap: &HashMap<(u64, String), usize>, gene_vector: Vec
 
     println!("building index");
     let cb_ix = all_cbs.iter().enumerate().map(|(ix, cb)|(cb.clone(), ix)).collect::<HashMap<_,_>>();
-    let gene_ix = gene_vector.iter().enumerate().map(|(ix, cb)|(cb.clone(), ix)).collect::<HashMap<_,_>>();
 
     // sparse matrix indices
     let mut ii: Vec<usize> = Vec::new();
     let mut jj: Vec<usize> = Vec::new();
     let mut vv: Vec<usize> = Vec::new();
 
-    for ((cb, gene), counter) in countmap{
+    for ((cb, geneid), counter) in countmap{
 
         let cbi = cb_ix.get(&cb).unwrap();
-        let genei = gene_ix.get(&gene).expect(&format!("{} missing", gene));
+        let genei = *geneid as usize;
         ii.push(*cbi);
-        jj.push(*genei);
+        jj.push(genei);
         vv.push(*counter);
     }
 
     let c: sprs::TriMat<usize> = sprs::TriMat::from_triplets(
-        (cb_ix.len(), gene_ix.len()),
+        (cb_ix.len(), gene_vector.len()),
         ii,
         jj,
         vv
@@ -83,10 +88,157 @@ fn countmap_to_matrix(countmap: &HashMap<(u64, String), usize>, gene_vector: Vec
     let b: sprs::CsMat<_> = c.to_csr();
 
     let cbs_seq: Vec<String> = all_cbs.into_iter().map(|x|int_to_seq(*x, 16)).collect();
-    let gene_seq: Vec<String> = gene_vector.into_iter().map(|x|x.clone()).collect();
-    let cmat = CountMatrix::new(b, cbs_seq, gene_seq);
+    // let gene_seq: Vec<String> = gene_vector.into_iter().map(|x|x.clone()).collect();
+    let gene_seq: Vec<String> = gene_vector.into_iter().collect(); //not sure if this does anything
+    
+    CountMatrix::new(b, cbs_seq, gene_seq)
 
-    cmat
+}
+
+
+pub fn baysian_count(bfolder: BusFolder, ignore_multimapped:bool, n_samples: usize){
+    let bfile = format!("{}/{}", bfolder.foldername, bfolder.busfile);
+    println!("{}",bfile);
+
+    let ec2gene = bfolder.ec2gene;
+
+    let cbumi_iter_tmp = CbUmiIterator::new(&bfile);
+    println!("determine size of iterator");
+    let now = Instant::now();
+    let total_records = cbumi_iter_tmp.count();
+
+    let cbumi_iter_tmp = CbUmiIterator::new(&bfile);
+    let max_length_records = cbumi_iter_tmp.map(|(_cbumi, rlist)| rlist.len()).max().unwrap();
+
+    let elapsed_time = now.elapsed();
+    println!("determined size of iterator {} in {:?}. Longest element: {} in a single CB/UMI", total_records, elapsed_time, max_length_records);
+
+    // handles the mapping between EC and gene
+    let egm = Ec2GeneMapper::new(ec2gene);
+
+
+    // prep for the multinomial sample
+    println!("Preparing the probability vector for mutlinomial");
+    let cbumi_iter_tmp = CbUmiIterator::new(&bfile);
+    let count_vec:Vec<_> = cbumi_iter_tmp
+        .flat_map(|(_cbumi, rlist)| rlist.into_iter().map(|r|r.COUNT as f64))
+        .collect();
+
+    let total_counts: f64 = count_vec.iter().sum();
+    let p_vec: Vec<f64> = count_vec.into_iter().map(|c| c/total_counts).collect();
+    println!("Done: {} rercods, {} counts", p_vec.len(), total_counts);
+
+    use probability::prelude::*;
+    let mut random_source = source::default(42);
+
+    let mut counter = 0;
+    for i in 0..n_samples{
+
+        // CB,gene_id -> count
+        let mut all_expression_vector: HashMap<(u64, u32), usize> = HashMap::new();
+        let mut n_mapped = 0;
+        let mut n_multi_inconsistent = 0;
+
+        // subsample the count vector
+        println!("Iteration {}: Mutlinomial sample", i);
+        let new_count_sample = multinomial_sample(total_counts as u64, &p_vec, &mut random_source);
+        println!("Done");
+
+        let cbumi_iter = CbUmiIterator::new(&bfile);
+        let now = Instant::now();
+        let bar = get_progressbar(total_records as u64);
+        let mut current_record_counter: usize = 0;
+
+        for ((cb, _umi), rlist) in cbumi_iter{
+
+            // inject the sampled numbers into the records
+
+            let indices = current_record_counter..current_record_counter+rlist.len();
+            let injected_counts: Vec<u32> = indices.map(|idx| *new_count_sample.index(idx) as u32 ).collect(); // wrning f64->u32
+            // let mut injected_records: Vec<BusRecord> = Vec::with_capacity(rlist.len());
+            let mut injected_records: Vec<BusRecord> = rlist.clone();
+
+            for i in 0..injected_records.len(){
+                // let mut r = injected_records.get_mut(i).expect(&format!("injected_records {}", i));
+                let mut r = injected_records.get_mut(i).unwrap_or_else(|| panic!("injected_records {}", i));
+                let c = injected_counts.get(i).unwrap_or_else(|| panic!("injected_counts {}", i));
+                r.COUNT = *c;
+            }
+
+            injected_records.retain(|r|r.COUNT>0);
+
+            // for (r, new_count) in injected_records.iter_mut().zip(injected_counts.into_iter()){
+            //     r.COUNT = new_count;
+            //     injected_records.push(r);
+            // }
+            current_record_counter += rlist.len();
+
+            if injected_records.is_empty(){
+                continue;
+            }
+
+
+            if let Some(g) = count_from_record_list(injected_records, &egm, ignore_multimapped){
+                // the records could be made into a single count for gene g
+                let key = (cb, g);
+                let current_count = all_expression_vector.entry(key).or_insert(0);
+                *current_count+=1;
+    
+                n_mapped += 1;
+            }
+            else{
+                // multimapped, or not consistently mapped
+                n_multi_inconsistent += 1;
+            }
+    
+            if counter % 1_000_000 == 0{
+                bar.inc(1_000_000);
+            }
+            counter += 1;
+        }
+
+        let elapsed_time = now.elapsed();
+        let fraction_mapped = n_multi_inconsistent as f64 /(n_mapped as f64+n_multi_inconsistent as f64);
+        println!("Iteration {}: Mapped {}, multi-discard {} ({}%) in {:?}",i, n_mapped, n_multi_inconsistent, 100.0*fraction_mapped, elapsed_time);
+
+        let ngenes = egm.int_to_gene.len();
+        let genelist_vector: Vec<String> = (0..ngenes).map(|k| egm.resolve_gene_id(k as u32)).collect();
+        // this is how genes are ordered as by EGM
+        // i.e. countmap[cb, i] corresponds to the number of count of genelist_vector[i]
+    
+        let countmatrix = countmap_to_matrix(&all_expression_vector, genelist_vector );
+        println!("{:?} nnz {}", countmatrix.matrix.shape(), countmatrix.matrix.nnz()); 
+        println!("finished iteration {}", i)         
+    }
+}
+
+
+
+fn count_from_record_list(records: Vec<BusRecord>, egmapper: &Ec2GeneMapper, ignore_multimapped:bool) -> Option<u32>{
+
+    // given a set of Records from the same CB/UMI, are they consistent with a particular gene
+    // which would yield a signel count
+    let consistent_genes: HashSet<u32>;
+
+    if !ignore_multimapped{
+            consistent_genes= find_consistent(&records, egmapper);
+    }else if records.len() > 1{
+        return None
+    }
+    else{
+        // consistent_genes = ec2gene.get(&records[0].EC).unwrap().clone();
+        consistent_genes = egmapper.get_genes(records[0].EC).clone();
+    }
+       
+    // uniquely mapped to a single gene
+    if consistent_genes.len() == 1{
+        let g = consistent_genes.into_iter().next().unwrap().clone();
+        Some(g)
+    }
+    else{
+        // multimapped or inconsistent
+        None
+    }
 
 }
 
@@ -110,82 +262,83 @@ pub fn count(bfolder: BusFolder, ignore_multimapped:bool) -> CountMatrix {
 
     let elapsed_time = now.elapsed();
 
-
     println!("determined size of iterator {} in {:?}. Longest element: {} in a single CB/UMI", total_records, elapsed_time, max_length_records);
 
     // handles the mapping between EC and gene
-    let EGM = Ec2GeneMapper::new(ec2gene);
+    let eg_mapper = Ec2GeneMapper::new(ec2gene);
 
 
-    // CB,gene -> count
+    // CB,gene_id -> count
     let mut all_expression_vector: HashMap<(u64, u32), usize> = HashMap::new();
     let bar = get_progressbar(total_records as u64);
 
     let mut n_mapped = 0;
     let mut n_multi_inconsistent = 0;
 
-    let mut counter = 0;
-
     let now = Instant::now();
 
-    for ((cb, _umi), record_list) in cbumi_iter {
-        let consistent_genes: HashSet<u32>;
+    for (counter, ((cb, _umi), record_list)) in cbumi_iter.enumerate() {
 
-        if !ignore_multimapped{
-                consistent_genes= find_consistent(&record_list, &EGM);
-        }else{
-            if record_list.len() > 1{
-                continue
-            }
-            else{
-                // consistent_genes = ec2gene.get(&record_list[0].EC).unwrap().clone();
-                consistent_genes = EGM.get_genes(record_list[0].EC).clone();
-            }
-        }
-       
-        // uniquely mapped to a single gene
-        if consistent_genes.len() == 1{
-            let g = consistent_genes.into_iter().next().unwrap().clone();
+        // try to map the records of this CB/UMI into a single gene
+        if let Some(g) = count_from_record_list(record_list, &eg_mapper, ignore_multimapped){
+            // the records could be made into a single count for gene g
             let key = (cb, g);
-            
             let current_count = all_expression_vector.entry(key).or_insert(0);
             *current_count+=1;
 
             n_mapped += 1;
         }
         else{
-            // multimapped or inconsistent
+            // multimapped, or not consistently mapped
             n_multi_inconsistent += 1;
         }
 
         if counter % 1_000_000 == 0{
             bar.inc(1_000_000);
         }
-        counter += 1;
     }
 
     let elapsed_time = now.elapsed();
     println!("Mapped {}, multi-discard {} in {:?}", n_mapped, n_multi_inconsistent, elapsed_time); 
 
-    // turn from geneid to String
-    let EEE: HashMap<(u64, String), usize> = all_expression_vector
-        .into_iter()
-        .map(|((cb, geneid), v)| ((cb, EGM.resolve_gene_id(geneid)), v)).collect();
+    let ngenes = eg_mapper.int_to_gene.len();
+    let genelist_vector: Vec<String> = (0..ngenes).map(|k| eg_mapper.resolve_gene_id(k as u32)).collect();
+    // this is how genes are ordered as by EGM
+    // i.e. countmap[cb, i] corresponds to the number of count of genelist_vector[i]
 
-    //collect all genes
-    // let mut genelist = HashSet::new(); 
-    // for glist in ec2gene.values(){
-    //     genelist.extend(glist)
-    // }
-    // let mut genelist_vector :Vec<&String>= genelist.into_iter().collect::<Vec<&String>>();
-    let ngenes = EGM.int_to_gene.len();
-    let genelist_vector: Vec<String> = (0..ngenes).map(|k| EGM.resolve_gene_id(k as u32)).collect();
-    let mut genelist_vector2 = genelist_vector.iter().collect::<Vec<&String>>();
+    let countmatrix = countmap_to_matrix(&all_expression_vector, genelist_vector );
 
-    genelist_vector2.sort();
-
-    let countmatrix = countmap_to_matrix(&EEE, genelist_vector2 );
-    println!("{:?} nnz {}", countmatrix.matrix.shape(), countmatrix.matrix.nnz());
+    let (shape1,shape2) = countmatrix.matrix.shape();
+    println!("{}x{} nnz {}\n\n\n",shape1, shape2 , countmatrix.matrix.nnz());
     countmatrix
 
+}
+
+
+#[cfg(test)]
+mod test{
+    use std::collections::HashMap;
+    use ndarray::{arr2};
+    use super::countmap_to_matrix;
+
+    #[test]
+    fn test_countmatrix(){
+
+        let mut countmap: HashMap<(u64, u32), usize> = HashMap::new();
+        countmap.insert((0, 0), 10);
+        countmap.insert((0, 1), 1);
+        countmap.insert((1, 0), 0);  // lets see what happens with empty counts
+        countmap.insert((1, 1), 5);
+
+        let gene_vector = vec!["geneA".to_string(), "geneB".to_string()];
+
+        let cmat = countmap_to_matrix(&countmap, gene_vector);
+
+        let dense_mat = cmat.matrix.to_dense();
+        let expected = arr2(&[[ 10, 1],
+                                                                  [ 0,  5]]);
+        assert_eq!(dense_mat, expected);
+
+        assert_eq!(cmat.cbs, vec!["AAAAAAAAAAAAAAAA".to_string(),"AAAAAAAAAAAAAAAC".to_string()]);
+    }
 }
--- src/multinomial.rs
+++ src/multinomial.rs
@@ -1,9 +1,13 @@
+use std::fs::File;
+use std::time::Instant;
+
 use statrs::distribution::{Multinomial, Binomial};  // warning the statrs::Binomial has very slow sampling (sum of Bernullis)
 use probability;  // use probability::distribution::Binomial instead, which does inverse cdf sampling
 use probability::distribution::Sample;
 use probability::prelude::*;
 use rand;
 use rand::distributions::Distribution;
+use probability::source::Xorshift128Plus;
 
 
 pub fn multinomial_sample_statrs(n: u64, pvec: Vec<f64>) -> Vec<f64>{
@@ -32,7 +36,11 @@ pub fn multinomial_sample_statrs(n: u64, pvec: Vec<f64>) -> Vec<f64>{
                 println!("{:?}", &pvec_norm[counter..]);
                 panic!("0<{}<1, counter {}", _ptmp, counter);
             }            
-            let b = Binomial::new(_ptmp, remaining_n).expect(&format!("p={} remaining_p={} ptmp={} n={}", p, remaining_p ,_ptmp, remaining_n)).sample(&mut r);
+            // let b = Binomial::new(_ptmp, remaining_n).expect(&format!("p={} remaining_p={} ptmp={} n={}", p, remaining_p ,_ptmp, remaining_n)).sample(&mut r);
+            let bin = Binomial::new(_ptmp, remaining_n).unwrap_or_else(|_| panic!("p={} remaining_p={} ptmp={} n={}", p, remaining_p ,_ptmp, remaining_n));
+            
+            // expect(&format!("p={} remaining_p={} ptmp={} n={}", p, remaining_p ,_ptmp, remaining_n)).sample(&mut r);
+            let b = bin.sample(&mut r);
 
             x.push(b);
             remaining_n -= b as u64;
@@ -42,15 +50,13 @@ pub fn multinomial_sample_statrs(n: u64, pvec: Vec<f64>) -> Vec<f64>{
     x
 }
 
-pub fn multinomial_sample(n: u64, pvec: Vec<f64>) -> Vec<f64>{
+pub fn multinomial_sample(n: u64, pvec: &Vec<f64>, source: &mut Xorshift128Plus) -> Vec<f64>{
     /*
     my own multinomial sampling, using the fact that all marginals are binomial
 
     statrs version does the same algorithm, but relies internally on a statrs::distribution::Binomial
     which is extremely slow.
     */
-    let mut source = source::default(42);
-
     let mut x :Vec<f64> = Vec::new();
 
     // normalize the pvec
@@ -76,7 +82,7 @@ pub fn multinomial_sample(n: u64, pvec: Vec<f64>) -> Vec<f64>{
                 println!("{:?}", &pvec_norm[counter..]);
                 panic!("0<{}<1, counter {}", _ptmp, counter);
             }
-            let btmp = probability::distribution::Binomial::new(remaining_n as usize, _ptmp ).sample(&mut source); //.expect(&format!("p={} remaining_p={} ptmp={} n={}", p, remaining_p ,_ptmp, remaining_n)).sample(&mut r);
+            let btmp = probability::distribution::Binomial::new(remaining_n as usize, _ptmp ).sample(source); //.expect(&format!("p={} remaining_p={} ptmp={} n={}", p, remaining_p ,_ptmp, remaining_n)).sample(&mut r);
             let b = btmp as f64;
 
 
@@ -88,14 +94,90 @@ pub fn multinomial_sample(n: u64, pvec: Vec<f64>) -> Vec<f64>{
     x
 }
 
+// use std::slice::binary_search;
+
+pub fn multinomial_sample_binary_search(n: u64, pvec: &Vec<f64>, source: &mut Xorshift128Plus) -> Vec<f64>{
+
+    // initialize to zero
+    let mut x: Vec<f64> = std::iter::repeat(0.0).take(pvec.len()).collect();
+
+    // normalize the pvec
+    let _sum: f64 = pvec.iter().sum();
+    let pvec_norm: Vec<f64> = pvec.iter().map(|x| x/_sum).collect();
+
+    let mut _acc = 0.0;
+    let cdf: Vec<f64> = pvec_norm
+        .iter()
+        .map(|x| {
+            _acc += x;
+            _acc
+        })
+        .collect();
+
+    // println!("{:?}", cdf);
+
+    let rv_uniform = Uniform::new(0.0, 1.0);
+    for _i in 0..n{
+        let r = rv_uniform.sample(source);
+        
+        // bit weird logic here: we probably wont find the exact value, so we'll never get Result::Ok
+        // but instead Result:Err will tell us the location wher to be inserted!
+        // 
+        let res = cdf.binary_search_by(|v| {
+            v.partial_cmp(&r).unwrap()});
+
+        let interval = match res{
+            Ok(index_exact_match) => {println!("Should not happen {}", index_exact_match); index_exact_match}
+            // [0.1, 0.30000000000000004, 0.6000000000000001, 1.0]
+            // r=0.12 ->  index= 1 i.e. the intervale 0.1-03
+            Err(first_index_larger_than_r) => first_index_larger_than_r
+        };
+        // println!("{} {}", r, interval);
+        x[interval] += 1.0;
+         
+    }
+    x
+    // println!("{:?}", x);
+
+}
+
+#[test]
+pub fn test_multinomial_binary(){
+    use std::time::Instant;
+
+    // Realistic sizes
+    // dim = 91_355_762 records,
+    // N   = 124_166_898 counts
+
+    // dim = 50_000_000;
+    // n = 100_000_000;
+    // takes about 380sec currently
+
+    let mut random_source = source::default(4);
+    let dim = 50_000_000;
+    let n = 100_000_000;
+    let p = (1..dim).map(|x| x as f64).collect();
+    // println!("{:?}", p);
+    let now = Instant::now();
+    let x = multinomial_sample_binary_search(n, &p, &mut random_source);
+    let elapsed_time = now.elapsed();
+     println!("Time for binary search N={} dim={}: {} secs", n, dim, elapsed_time.as_secs());
+
+     let s:f64 = x.iter().fold(0.0, |acc, el| acc+*el);
+     assert_eq!(n as f64, s)
+}
+
 // #[test]
 pub fn test_multinomial(dim: i32){
+
+    let mut random_source = source::default(42);
+
     let n = 1000;
     let p = (1..dim).map(|x| x as f64).collect();
     // println!("{:?}", p);
 
-    let x = multinomial_sample(n, p);
-    let n2: f64 = x.iter().sum();
+    let x = multinomial_sample(n, &p, &mut random_source);
+    let _n2: f64 = x.iter().sum();
     // println!("{:?} {}", x, n2);
 }
 // #[test]
@@ -107,7 +189,79 @@ pub fn test_multinomial_stats(dim: i32){
     let mut r = rand::thread_rng();
     let n = Multinomial::new(&p, n).unwrap();
     let x = n.sample(&mut r);
-    let n2: f64 = x.iter().sum();
+    let _n2: f64 = x.iter().sum();
     // println!("{:?} {}", x, n2);
 
+}
+
+#[test]
+pub fn multinomial_speed_eval(){
+
+    use core::ops::Add;
+    use core::ops::Div;
+    use core::ops::Sub;
+    use std::fmt::Debug;
+    
+    pub fn linspace<T>(x0: T, xend: T, n: u16) -> Vec<T>
+    where
+        T: Sub<Output = T> + Add<Output = T> + Div<Output = T> + Clone + Debug,
+        u16: TryInto<T> + TryInto<usize>,
+        <u16 as TryInto<T>>::Error: Debug,
+    {
+        let segments: T = (n - 1)
+            .try_into()
+            .expect("requested number of elements did not fit into T");
+        let n_size: usize = n.try_into()
+            .expect("requested number of elements exceeds usize");
+            
+        let dx = (xend - x0.clone()) / segments;
+    
+        let mut x = vec![x0; n_size];
+    
+        for i in 1..n_size {
+            x[i] = x[i - 1].clone() + dx.clone();
+        }
+    
+        x
+    }
+    
+    // let dims = vec![1_000, 10_000, 100_000, 1_000_000];
+    // let N: Vec<usize> = vec![1_000, 10_000, 100_000, 1_000_000];
+
+    let dims: Vec<usize> = linspace(4.0,7.0, 20).iter().map(|x| 10_f64.powf(*x) as usize).collect();
+    let N: Vec<usize> = linspace(4.0,7.0, 20).iter().map(|x| 10_f64.powf(*x) as usize).collect();
+
+    println!("{:?}", dims);
+
+    let mut random_source = source::default(42);
+
+    struct Res {
+        n: usize,
+        d: usize,
+        time: f32
+    }
+
+    let mut results:Vec<Res> = Vec::new();
+    for n in N{
+        for d in dims.clone(){
+
+            println!("n={} d={}", n, d);
+            let pvec: Vec<_> = (1..d).map(|x| x as f64).collect();
+
+            let now = Instant::now();
+            let _x = multinomial_sample_binary_search(n as u64, &pvec, &mut random_source);
+            let elapsed_time = now.elapsed().as_secs_f32();
+
+            let r = Res{n, d, time:elapsed_time};
+            results.push(r)
+            }
+    }
+
+    let mut fh = File::create("/tmp/multinom_speed.csv").unwrap();
+    use std::io::Write;
+    write!(fh, "N,d,time\n").unwrap();
+
+    for r in results{
+        write!(fh, "{},{},{}\n", r.n, r.d, r.time).unwrap();
+    }
 }
