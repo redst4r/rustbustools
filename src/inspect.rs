70e04b7154a3318dca5074d14180015c06dd0925
redst4r <redst4r@web.de>
2023-06-02 17:26:10 -0700

    generalized "inspect()"

--- src/inspect.rs
+++ src/inspect.rs
@@ -3,7 +3,15 @@ use crate::{
     iterators::{CbUmiGroupIterator, CellGroupIterator},
 };
 
-pub fn inspect(busfile: &str) {
+#[derive(Debug, Eq, PartialEq)]
+struct BusStatistics {
+    nrecords: usize,
+    nreads: usize,
+    n_cells: usize,
+    n_cbumi: usize,
+}
+
+fn _inspect(busfile: &str) -> BusStatistics {
     let n_cbumi = BusReader::new(busfile).groupby_cbumi().count();
     let n_cells = BusReader::new(busfile).groupby_cb().count();
 
@@ -13,18 +21,23 @@ pub fn inspect(busfile: &str) {
     let bus = BusReader::new(busfile);
     for r in bus {
         nrecords += 1;
-        nreads += r.COUNT
+        nreads += r.COUNT as usize
     }
 
-    println!("{nrecords} BUS records");
-    println!("{nreads} reads");
-    println!("{n_cells} cell-barcodes");
-    println!("{n_cbumi} CB-UMIs");
+    BusStatistics {nrecords, nreads ,  n_cells, n_cbumi}
+}
+
+pub fn inspect(busfile: &str) {
+    let stats = _inspect(busfile);
+    println!("{} BUS records", stats.nrecords);
+    println!("{} reads", stats.nreads);
+    println!("{} cell-barcodes", stats.n_cells);
+    println!("{} CB-UMIs", stats.n_cbumi);
 }
 
 #[cfg(test)]
 mod testing {
-    use super::inspect;
+    use super::{_inspect, BusStatistics};
     use crate::io::{setup_busfile, BusRecord};
 
     #[test]
@@ -35,12 +48,17 @@ mod testing {
         let r4 = BusRecord{CB: 2, UMI: 1, EC: 1, COUNT: 2, FLAG: 0};
         let r5 = BusRecord{CB: 2, UMI: 21, EC: 1, COUNT: 2, FLAG: 0};
         let r6 = BusRecord{CB: 3, UMI: 1, EC: 1, COUNT: 2, FLAG: 0};
+        let r7 = BusRecord{CB: 3, UMI: 1, EC: 10, COUNT: 2, FLAG: 0};
 
-        let records = vec![r1.clone(),r2.clone(),r3.clone(),r4.clone(),r5.clone(), r6.clone()];
+        let records = vec![r1.clone(),r2.clone(),r3.clone(),r4.clone(),r5.clone(), r6.clone(), r7.clone()];
         // let records = vec![r1,r2,r3,r4,r5, r6].to_vec();
 
         let (busname, _dir) = setup_busfile(&records);
 
-        inspect(&busname);
+        let r = _inspect(&busname);
+        assert_eq!(
+            r,
+            BusStatistics { nrecords: 7, nreads: 34, n_cells: 4, n_cbumi: 6 }
+        );
     }
 }
