d16e2956ac042021eb417cc36adef0241a16f130
redst4r <redst4r@web.de>
2023-07-20 09:12:59 -0700

    runlength_encoding: optinal shift

--- src/runlength_codec.rs
+++ src/runlength_codec.rs
@@ -9,7 +9,8 @@
 /// Encoding will yield a stream of u64s, encoding either (RLE_item, runlength) or (items)
 pub struct RunlengthCodec {
     #[allow(non_snake_case)]
-    pub RLE_VAL : u64 // 
+    pub RLE_VAL : u64,
+    pub shift_up_1: bool //whether to shift encoded values +1; usefull in conjunction with fib-enc
 }
 impl RunlengthCodec {
     // this currenly shifts all values by +1 (since the subsequent fibonacci encoding cant handle 0)
@@ -26,22 +27,32 @@ impl RunlengthCodec {
                 // we're encoding a runlength (char 0 for x times)
                 if runlen > 0 {
                     // encode the value
-                    encoded.push(self.RLE_VAL+1); // since we cant encode zero 
-    
+                    if self.shift_up_1{
+                        encoded.push(self.RLE_VAL+1); // since we cant encode zero 
+                    } else {
+                        encoded.push(self.RLE_VAL); // since we cant encode zero 
+                    }
                     // encode the runlength
                     encoded.push(runlen);
     
                     runlen = 0
                 }
                 // we're just encoding a single value
+                if self.shift_up_1{
                 encoded.push(x+1); // since we cant encode zero 
+                } else {
+                    encoded.push(x); // since we cant encode zero 
+                }
             }
         }
         // println!("Aftermath");
         if runlen >0 {
             // encode the value
-            encoded.push(self.RLE_VAL+1); // since we cant encode zero 
-    
+            if self.shift_up_1{
+                encoded.push(self.RLE_VAL+1); // since we cant encode zero 
+            } else {
+                encoded.push(self.RLE_VAL); // since we cant encode zero 
+            }    
             // encode the runlength
             encoded.push(runlen);    
         }
@@ -54,15 +65,17 @@ impl RunlengthCodec {
         let mut iii = input.iter();
         loop {
             // if there's still some item in the stream
-            if let Some(item) = iii.next() {
+            if let Some(&item) = iii.next() {
                 // println!("{}", item);
-                if item == &(self.RLE_VAL+ 1) { // everything is shifted by 1 in the encoded stream
+                let adjusted_item = if self.shift_up_1 { item -1} else {item};
+
+                if adjusted_item == (self.RLE_VAL) { // everything is shifted by 1 in the encoded stream
                     let runlen = *iii.next().unwrap(); // this shouldnt fail, each RLE is followed by runlen
                     for _ in 0..runlen {
                         decoded.push(self.RLE_VAL);  // shifting the value by -1, or equivalently use RLE
                     }
                 } else {
-                    decoded.push(*item-1)
+                    decoded.push(adjusted_item)
                 }
             }
             else{
@@ -80,7 +93,7 @@ mod test {
     #[test]
     fn test_encode_decode_single_run(){
         // a CODEC which compresses runs of thevalue 0
-        let c = RunlengthCodec { RLE_VAL: 0 };
+        let c = RunlengthCodec { RLE_VAL: 0, shift_up_1: true };
 
         let plain = vec![0,0,0];
         let enc = c.encode(plain.clone().into_iter());
@@ -92,7 +105,7 @@ mod test {
     #[test]
     fn test_encode_decode_no_run(){
         // a CODEC which compresses runs of thevalue 0
-        let c = RunlengthCodec { RLE_VAL: 0 };
+        let c = RunlengthCodec { RLE_VAL: 0, shift_up_1: true };
         let plain = vec![1,2,1];
         let enc = c.encode(plain.clone().into_iter());
         assert!(plain.len()== enc.len()); // cant be compressed
@@ -103,7 +116,7 @@ mod test {
     #[test]
     fn test_encode_decode_minxed_run(){
         // a CODEC which compresses runs of thevalue 0
-        let c = RunlengthCodec { RLE_VAL: 0 };
+        let c = RunlengthCodec { RLE_VAL: 0, shift_up_1: true };
         let plain = vec![1,0,0, 2,0,1,0];
         let enc = c.encode(plain.clone().into_iter());
         assert!(plain.len()< enc.len()); // here the encoding is acually longer! dueto the freqeunt 1-runs of zeros
@@ -115,10 +128,38 @@ mod test {
     #[test]
     fn test_encode_decode_minxed_run_rle1(){
         // a CODEC which compresses runs of thevalue 0
-        let c = RunlengthCodec { RLE_VAL: 1 };
+        let c = RunlengthCodec { RLE_VAL: 1, shift_up_1: true };
         let plain = vec![0,1,1, 1,1, 0];
         let enc = c.encode(plain.clone().into_iter());
         let dec = c.decode(dbg!(enc));
         assert_eq!(plain, dec)
     }
+
+    #[test]
+    fn test_encode_decode_minxed_run_rle1_1(){
+        let c = RunlengthCodec { RLE_VAL: 1 , shift_up_1: true};
+        let plain = vec![0,1,1,];
+        let enc = c.encode(plain.clone().into_iter());
+        let dec = c.decode(dbg!(enc));
+        assert_eq!(plain, dec)
+    }
+
+
+    #[test]
+    fn test_encode_decode_no_shift(){
+        let c = RunlengthCodec { RLE_VAL: 0 , shift_up_1: false};
+        let plain = vec![0,0,1,1,];
+        let enc = c.encode(plain.clone().into_iter());
+        let dec = c.decode(dbg!(enc));
+        assert_eq!(plain, dec)
+    }
+    #[test]
+    fn test_encode_decode_no_shift_2(){
+        let c = RunlengthCodec { RLE_VAL: 1 , shift_up_1: false};
+        let plain = vec![0,0,1,1,];
+        let enc = c.encode(plain.clone().into_iter());
+        let dec = c.decode(dbg!(enc));
+        assert_eq!(plain, dec)
+    }
+
 }
